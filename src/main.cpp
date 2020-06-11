273b62903d8f7d4a2a1b5251c51eeff5ccdb234e
cartesiam <contact@cartesiam.com>
2020-06-11 17:51:11 +0200

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
-/********************************* Libraries *********************************/
+/**
+*******************************************************************************
+* @file   main.cpp
+* @brief  Main program body
+*******************************************************************************
+* Main program for collecting data and testing NanoEdge AI solution
+*
+* Compiler Flags
+* -DDATA_LOGGING : data logging mode for collecting data
+* -DNEAI_LIB     : test mode with NanoEdge AI Library
+*
+* @note   if no compiler flag then data logging mode by default
+*******************************************************************************
+*/
+
+/* Includes ------------------------------------------------------------------*/
 #include "mbed.h"
 #include "LSM6DSLSensor.h"
 
-#if !defined(DATA_LOGGING) && !defined(NEAI_LIB)
+// In case there is no compiler flag, we set DATA_LOGGING
+#ifndef NEAI_LIB
 #define DATA_LOGGING
 #endif
 
@@ -11,11 +27,12 @@
 #endif
 
 /* Defines -------------------------------------------------------------------*/
+
 #ifdef DATA_LOGGING
 #define DATA_INPUT_USER 		1024
 #define AXIS_NUMBER 			3
 #else
-#define LEARNING_NUMBER 		30 		/* Number of learning signals */
+#define LEARNING_NUMBER 		5 		/* Number of learning signals */
 #endif
 
 #define MINI 					5
@@ -26,12 +43,12 @@
 /* Objects -------------------------------------------------------------------*/
 
 Serial pc (USBTX, USBRX);
-SPI spi(D11, D12, D13); // mosi, miso, sclk
+SPI spi(A6, A5, A4); // mosi, miso, sclk
 DigitalOut cs(A3, 0);
+DigitalOut d1(D2, 0); // D2 = blue
+DigitalOut d2(D3, 0); // D3 = red
+DigitalOut d3(D9, 0); // D9 = green
 LSM6DSLSensor *lsm6dsl = new LSM6DSLSensor(&spi, A3);
-DigitalOut led(LED1);
-
-
 
 /********************************* Prototypes *********************************/
 void init(void);
@@ -40,6 +57,10 @@ void data_logging_mode(void);
 #endif
 #ifdef NEAI_LIB
 void neai_library_test_mode(void);
+void led_anomaly(void);
+void led_nominal(void);
+void led_learning_over(void);
+void led_learned(void);
 #endif
 void get_values(void);
 void fill_acc_array(void);
@@ -70,7 +91,8 @@ int main()
 /********************************* Functions *********************************/
 void init ()
 {
-	pc.baud(115200);
+    pc.baud(115200);
+	wait_ms(100);
     lsm6dsl->set_x_odr(3330.0f);
 	lsm6dsl->set_x_fs(4.0f);
 	lsm6dsl->enable_x();
@@ -109,18 +131,20 @@ void data_logging_mode()
 void neai_library_test_mode()
 {
 	uint16_t learn_cpt = 0;
+	uint16_t similarity = 0;
 	do {
 		// Here we are polling in order to detect strumming vibration.
 		// Depending on your setup and instrument, edit the trigger function as needed. 
 		if (strum_trigger()) {
 			fill_acc_array();
 			NanoEdgeAI_learn(data_user);
+			led_learned();
 			pc.printf("%d\n", (int)(learn_cpt * 100) / LEARNING_NUMBER);
 			learn_cpt++;
 		}
-	} while (learn_cpt < LEARNING_NUMBER)
+	} while (learn_cpt < LEARNING_NUMBER);
 
-	led_blink_learning_over()
+	led_learning_over();
 
 	while(1) {
 		if (strum_trigger()) {
@@ -129,12 +153,12 @@ void neai_library_test_mode()
 			pc.printf("%d\n", similarity);
 
 			if (similarity < THRESH_SIMILARITY) {
-				led_blink_burst_ko();
+				led_anomaly();
 			} else {
-				led_blink_ok();
+				led_nominal();
 			}
 		}
-	}
+	}	
 }
 #endif
 
@@ -199,8 +223,8 @@ void fill_acc_array ()
 		data_user[(AXIS_NUMBER * i) + 2] = values[2];
 	}
 	/* Print data in the serial */
-#ifdef NEAI_DATALOGGING
-	for (uint16_t i = 0; i < DATA_INPUT_USER * AXIS_NUMBERS; i++) {
+#ifdef DATA_LOGGING
+	for (uint16_t i = 0; i < DATA_INPUT_USER * AXIS_NUMBER; i++) {
 		pc.printf("%.3f ", data_user[i]);
 	}
 	pc.printf("\n");
@@ -223,32 +247,44 @@ void get_values ()
 	}
 }
 
-void led_blink_burst_ko()
+void led_anomaly()
 {
-	for (uint8_t j = 0; j < 5; j++) {
-		led = 1;
-		wait_ms(200);
-		led = 0;
-		wait_ms(200);
+	for (uint8_t j = 0; j < 3; j++) {
+		d2 = 1;
+		wait_ms(100);
+		d2 = 0;
+		wait_ms(50);
 	}
 
 }
-void led_blink_learning_over()
+
+void led_learned()
 {
-	for (uint8_t j = 0; j < 5; j++) {
-		led = 1;
-		wait_ms(1000);
-		led = 0;
-		wait_ms(250);
+	for (uint8_t j = 0; j < 1; j++) {
+		d1 = 1;
+		wait_ms(750);
+		d1 = 0;
+		wait_ms(150);
+	}
+}
+
+void led_learning_over()
+{
+	for (uint8_t j = 0; j < 3; j++) {
+		d1 = 1;
+		wait_ms(750);
+		d1 = 0;
+		wait_ms(50);
 	}
 
 }
-void led_blink_ok()
+
+void led_nominal()
 {
-	for (uint8_t j = 0; j < 2; j++) {
-		led = 1;
+	for (uint8_t j = 0; j < 1; j++) {
+		d3 = 1;
 		wait_ms(1000);
-		led = 0;
+		d3 = 0;
 		wait_ms(250);
 	}
 
